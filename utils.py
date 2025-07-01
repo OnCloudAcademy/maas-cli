ada23648e0b5362848b8dbad610618cfe2c4ebd4
Raja Mohammad S <external.raja.mohammad@sky.de>
2025-07-01 22:26:12 +0530

    updated

--- utils.py
+++ utils.py
@@ -1,30 +1,35 @@
-
 import subprocess
 import os
-import shutil
+import tempfile
 
 TEMPLATE_REPO = "https://github.com/OnCloudAcademy/common-infra-template.git"
-TEMPLATE_DIR = "common-infra-template"
 
 def run_terraform(project_name, location, subscription_id, github_token, github_org):
-    # Clone repo
-    if os.path.exists(TEMPLATE_DIR):
-        shutil.rmtree(TEMPLATE_DIR)
-    subprocess.run(["git", "clone", TEMPLATE_REPO], check=True)
+    # Create a temporary directory (auto-cleans when done)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        print(f"📦 Cloning into temporary directory: {temp_dir}")
 
-    os.chdir(TEMPLATE_DIR)
+        # Clone the Terraform repo into the temp directory
+        subprocess.run(["git", "clone", TEMPLATE_REPO, temp_dir], check=True)
 
-    # Set subscription (optional)
-    subprocess.run(["az", "account", "set", "--subscription", subscription_id], check=True)
+        # Change to the cloned directory
+        os.chdir(temp_dir)
 
-    # Terraform init and apply
-    subprocess.run(["terraform", "init"], check=True)
-    subprocess.run([
-        "terraform", "apply",
-        "-auto-approve",
-        f"-var=project_name={project_name}",
-        f"-var=location={location}",
-        f"-var=subscription_id={subscription_id}",
-        f"-var=github_token={github_token}",
-        f"-var=github_org={github_org}"
-    ], check=True)
+        # Set the Azure subscription (optional but useful)
+        subprocess.run(["az", "account", "set", "--subscription", subscription_id], check=True)
+
+        # Initialize Terraform
+        subprocess.run(["terraform", "init"], check=True)
+
+        # Apply Terraform using provided variables
+        subprocess.run([
+            "terraform", "apply",
+            "-auto-approve",
+            f"-var=project_name={project_name}",
+            f"-var=location={location}",
+            f"-var=subscription_id={subscription_id}",
+            f"-var=github_token={github_token}",
+            f"-var=github_org={github_org}"
+        ], check=True)
+
+        # Temporary directory will be auto-deleted after this block
